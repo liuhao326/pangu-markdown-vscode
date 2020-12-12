fbf5c83dd17970c25b48f88c28f02b2bc92972d2
Liuhao <326047736@qq.com>
2020-12-12 21:03:59 +0800

    update

--- extension.js
+++ extension.js
@@ -1,32 +1,26 @@
+// @ts-nocheck
 /* jshint -W032 */ //关闭 jshint W032 的检查
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  * 方法被激活时调用的函数
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "pangu-markdown-vscode" is now active!');
-
 	let format = vscode.commands.registerCommand('pangu-markdown-vscode.formatPangu', () => {
 		new PanguFormatter().updateDocument();
 	});
 	context.subscriptions.push(format);
-
 	context.subscriptions.push(new Watcher());
 }
 exports.activate = activate;
 
 // 方法去激活时调用的函数
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
@@ -34,25 +28,30 @@ module.exports = {
 };
 
 class PanguFormatter {
+	constructor() {
+		this.config = vscode.workspace.getConfiguration('pangu-markdown-vscode')
+		this.CJK = '\\u2e80-\\u2eff\\u2f00-\\u2fdf\\u3040-\\u309f\\u30a0-\\u30fa\\u30fc-\\u30ff\\u3100-\\u312f\\u3200-\\u32ff\\u3400-\\u4dbf\\u4e00-\\u9fff\\uf900-\\ufaff'
+	};
+
 	updateDocument() {
+		//TODO：执行结束后选择文本或是光标位置不对的问题
 		let editor = vscode.window.activeTextEditor;
 		let doc = editor.document;
-		
 		if (doc.languageId === "markdown") {
-
 			vscode.window.activeTextEditor.edit((editorBuilder) => {
-				let content = doc.getText(this.current_document_range(doc));
+				let text = doc.getText(this.current_document_range(doc));
 
 				/* 全局替换 */
 				// 全角数字 ——> 半角数字
-				content = this.replaceFullNums(content);
-				// 全角英文和＠ ——> 半角英文和@
-				content = this.replaceFullChars(content);
+				text = this.replaceFullNums(text);
+				// 全角英文 ——> 半角英文
+				text = this.replaceFullChars(text);
 				// 删除多余的内容（回车）
-				content = this.condenseContent(content);
+				text = this.condenseContent(text);
 
 				/* 每行操作 */
-				content = content.split("\n").map((line) => {
+				text = text.split("\n").map((line) => {
+					// 处理标点
 					line = this.replacePunctuations(line);
 					// 删除多余的空格
 					line = this.deleteSpaces(line);
@@ -61,83 +60,92 @@ class PanguFormatter {
 					return line;
 				}).join("\n");
 
-				editorBuilder.replace(this.current_document_range(doc), content);
+				properNounsAndAbbreviations();
+
+				editorBuilder.replace(this.current_document_range(doc), text);
 			});
 		} else {
-			vscode.window.showErrorMessage('不能处理非 Markdown 格式的文件。');
+			vscode.window.showInformationMessage('不能处理非 Markdown 格式的文件。');
 		};
 	};
 
-	deleteSpaces(content) {
+	/* 全角标点与其他字符之间不加空格 */
+	deleteSpaces(text) {
 		// 去掉「`()[]{}<>'"`」: 前后多余的空格
-		content = content.replace(/\s+([\(\)\[\]\{\}<>'":])\s+/g, ' $1 ');
-		
+		text = text.replace(/\s+([\(\)\[\]\{\}<>'":])\s+/g, ' $1 ');
+
 		// 去掉连续括号增加的空格，例如：「` ( [ { <  > } ] ) `」
-		content = content.replace(/([<\(\{\[])\s([<\(\{\[])\s/g,"$1$2 ");
-		content = content.replace(/([<\(\{\[])\s([<\(\{\[])\s/g,"$1$2 ");
-		content = content.replace(/([<\(\{\[])\s([<\(\{\[])\s/g,"$1$2 ");
-		content = content.replace(/([<\(\{\[])\s([<\(\{\[])\s/g,"$1$2 ");
-		content = content.replace(/\s([>\)\]\}])\s([>\)\]\}])/g," $1$2");
-		content = content.replace(/\s([>\)\]\}])\s([>\)\]\}])/g," $1$2");
-		content = content.replace(/\s([>\)\]\}])\s([>\)\]\}])/g," $1$2");
-		content = content.replace(/\s([>\)\]\}])\s([>\)\]\}])/g," $1$2");
-		
+		text = text.replace(/([<\(\{\[])\s([<\(\{\[])\s/g, "$1$2 ");
+		text = text.replace(/([<\(\{\[])\s([<\(\{\[])\s/g, "$1$2 ");
+		text = text.replace(/([<\(\{\[])\s([<\(\{\[])\s/g, "$1$2 ");
+		text = text.replace(/([<\(\{\[])\s([<\(\{\[])\s/g, "$1$2 ");
+		text = text.replace(/\s([>\)\]\}])\s([>\)\]\}])/g, " $1$2");
+		text = text.replace(/\s([>\)\]\}])\s([>\)\]\}])/g, " $1$2");
+		text = text.replace(/\s([>\)\]\}])\s([>\)\]\}])/g, " $1$2");
+		text = text.replace(/\s([>\)\]\}])\s([>\)\]\}])/g, " $1$2");
+
 		// 去掉 「`$ () $`」, 「`$ [] $`」, 「`$ {} $`」 里面增加的空格
 		// 去掉开始 $ 后面增加的空格，结束 $ 前面增加的空格
 		// 去掉包裹代码的符号里面增加的空格
 		// 去掉开始 ` 后面增加的空格，结束 ` 前面增加的空格
-		content = content.replace(/([`\$])\s*([<\(\[\{])([^\$]*)\s*([`\$])/g, "$1$2$3$4");
-		content = content.replace(/([`\$])\s*([^\$]*)([>\)\]\}])\s*([`\$])/g, "$1$2$3$4");
+		text = text.replace(/([`\$])\s*([<\(\[\{])([^\$]*)\s*([`\$])/g, "$1$2$3$4");
+		text = text.replace(/([`\$])\s*([^\$]*)([>\)\]\}])\s*([`\$])/g, "$1$2$3$4");
 
 		// 去掉「`) _`」、「`) ^`」增加的空格
-		content = content.replace(/\)\s([_\^])/g,")$1");
-		
+		text = text.replace(/\)\s([_\^])/g, ")$1");
+
 		// 去掉 [^footnote,2002] 中的空格
-		content = content.replace(/\[\s*\^([^\]\s]*)\s*\]/g, "[^$1]");
+		text = text.replace(/\[\s*\^([^\]\s]*)\s*\]/g, "[^$1]");
 
 		// 将链接的格式中文括号“[]（）”改成英文括号“[]()”，去掉增加的空格
-		content = content.replace(/\s*\[\s*([^\]]+)\s*\]\s*[（(]\s*([^\s\)]*)\s*[)）]\s*/g, " [$1]($2) ");
-        
-        // 将图片链接的格式中的多余空格“! []()”去掉，变成“![]()”
-		content = content.replace(/!\s*\[\s*([^\]]+)\s*\]\s*[（(]\s*([^\s\)]*)\s*[)）]\s*/g, "![$1]($2) ");
+		text = text.replace(/\s*\[\s*([^\]]+)\s*\]\s*[（(]\s*([^\s\)]*)\s*[)）]\s*/g, " [$1]($2) ");
+
+		// 将图片链接的格式中的多余空格“! []()”去掉，变成“![]()”
+		text = text.replace(/!\s*\[\s*([^\]]+)\s*\]\s*[（(]\s*([^\s\)]*)\s*[)）]\s*/g, "![$1]($2) ");
 
 		// 将网络地址中“ : // ”符号改成“://”
-		content = content.replace(/\s*:\s*\/\s*\/\s*/g, "://");
+		text = text.replace(/\s*:\s*\/\s*\/\s*/g, "://");
 
 		// 去掉行末空格
-		content = content.replace(/(\S*)\s*$/g, '$1');
+		text = text.replace(/(\S*)\s*$/g, '$1');
 
 		// 去掉「123 °」和 「15 %」中的空格
-		content = content.replace(/([0-9])\s*([°%])/g, '$1$2');
+		text = text.replace(/([0-9])\s*([°%])/g, '$1$2');
 
 		// 去掉 2020 - 04 - 20, 08 : 00 : 00 这种日期时间表示的数字内的空格
-		content = content.replace(/([0-9])\s*-\s*([0-9])/g, "$1-$2");
-		content = content.replace(/([0-9])\s*:\s*([0-9])/g, "$1:$2");
+		text = text.replace(/([0-9])\s*-\s*([0-9])/g, "$1-$2");
+		text = text.replace(/([0-9])\s*:\s*([0-9])/g, "$1:$2");
 
 		// 去掉 1 , 234 , 567 这种千分位表示的数字内的空格
-		content = content.replace(/([0-9])\s*,\s*([0-9])/g, "$1,$2");
+		text = text.replace(/([0-9])\s*,\s*([0-9])/g, "$1,$2");
 
 		// 全角標點與其他字符之間不加空格
 		// 将无序列表的-后面的空格保留
 		// 将有序列表的-后面的空格保留
-		content = content.replace(/^(?<![-|\d.]\s*)\s*([，。、《》？『』「」；∶【】｛｝—！＠￥％…（）])\s*/g, "$1");
-		return content;
+		text = text.replace(/^(?<![-|\d.]\s*)\s*([，。、《》？『』「」；∶【】｛｝—！＠￥％…（）])\s*/g, "$1");
+		return text;
 	};
 
-	insertSpace(content) {
+	/* 
+	中英文之间（「豆瓣FM」等产品名词，按照官方所定义的格式书写）；
+	中文与数字之间；
+	数字与单位之间（度 / 百分比与数字之间不需要增加空格。）；
+	链接之间增加空格（争议） */
+	insertSpace(text) {
 		// 在 “中文English” 之间加入空格 “中文 English”
 		// 在 “中文123” 之间加入空格 “中文 123”
-		content = content.replace(/([\u4e00-\u9fa5\u3040-\u30FF])([a-zA-Z0-9`])/g, '$1 $2');
+		//TODO：更完善的中文字符集
+		text = text.replace(/([\u4e00-\u9fa5\u3040-\u30FF])([a-zA-Z0-9`])/g, '$1 $2');
 
 		// 在 “English中文” 之间加入空格 “English 中文”
 		// 在 “123中文” 之间加入空格 “123 中文”
-		content = content.replace(/([a-zA-Z0-9%`])([*]*[\u4e00-\u9fa5\u3040-\u30FF])/g, "$1 $2");
+		text = text.replace(/([a-zA-Z0-9%`])([*]*[\u4e00-\u9fa5\u3040-\u30FF])/g, "$1 $2");
 
 		// 在 「100Gbps」之间加入空格「100 Gbps」（只有手工做，不能自动做，会破坏密码网址等信息）
-		
+
 		// 在 「I said:it's a good news」的冒号与英文之间加入空格 「I said: it's a good news」
-		content = content.replace(/([:])\s*([a-zA-z])/g, "$1 $2");
-		return content;
+		text = text.replace(/([:])\s*([a-zA-z])/g, "$1 $2");
+		return text;
 	};
 
 	// 获取可编辑文档的全部内容
@@ -148,159 +156,157 @@ class PanguFormatter {
 		return range;
 	};
 
-	condenseContent(content) {
+	/* 删除多余空行；tabs ——> 四个空格 */
+	condenseContent(text) {
 		// 制表符 ——> 四个空格
-		let config = vscode.workspace.getConfiguration('pangu-markdown-vscode')
-		if(config.get('covert_tabs_to_whitespace')){
-			content = content.replace(/\t/g, "    ");
+		let config = this.config
+		if (config.get('covert_tabs_to_whitespace')) {
+			text = text.replace(/\t/g, "    ");
 		}
 		// 删除超过2个的回车
 		// Unix 的只有 LF，Windows 的需要 CR LF
-		if(config.get('delete_extra_blank_lines')){
-			content = content.replace(/(\n){3,}/g, "$1$1");
-			content = content.replace(/(\r\n){3,}/g, "$1$1");
+		if (config.get('delete_extra_blank_lines')) {
+			text = text.replace(/(\n){3,}/g, "$1$1");
+			text = text.replace(/(\r\n){3,}/g, "$1$1");
 			//删除文档结尾处多余的换行
-			content = content.replace(/(\n){2,}$/g, '');
-			content = content.replace(/(\r\n){2,}$/g, '');
+			text = text.replace(/(\n){2,}$/g, '');
+			text = text.replace(/(\r\n){2,}$/g, '');
 		}
-		return content;
+		return text;
 	};
 
-	replacePunctuations(content) {
-		let config = vscode.workspace.getConfiguration('pangu-markdown-vscode')
+	/* 
+	使用全角中文标点；
+	遇到完整的英文整句、特殊名词，其内容使用半角标点;
+	简体中文使用直角引号（争议）；
+	不重复使用标点符号；
+	*/
+	replacePunctuations(text) {
+		/* 思路：转换必定要转为全角标点的半角标点 ——> 处理连续标点 ——> 引号转为直角引号 ——> 打印提示，使用者手动检查需要转为半角的全角及需要转为全角的半角标点 */
 
-		//TODO：更完善的中文字符集
-		//TODO：考虑元字符
-		// 中文结尾处的 `, \ . : ; ? !` 改成 `，、。：；？！`
-		content = content.replace(/([\u4e00-\u9fa5\u3040-\u30FF])\.($|\s*)/g, '$1。');
-		content = content.replace(/([\u4e00-\u9fa5\u3040-\u30FF]),/g, '$1，');
-		content = content.replace(/([\u4e00-\u9fa5\u3040-\u30FF]);/g, '$1；');
-		content = content.replace(/([\u4e00-\u9fa5\u3040-\u30FF])!/g, '$1！');
-		content = content.replace(/([\u4e00-\u9fa5\u3040-\u30FF])\?/g, '$1？');
-		content = content.replace(/([\u4e00-\u9fa5\u3040-\u30FF])\\/g, '$1、');
-		content = content.replace(/([\u4e00-\u9fa5\u3040-\u30FF])\:/g, '$1：');
+		// 中文结尾处的半角标点 ——> 全角标点
+		let CJK = this.CJK
+		text = text.replace(new RegExp("([{CJK}])\\.([{CJK}A-Za-z0-9])".replace(/\{CJK\}/g, CJK), "g"), '$1。$2');// .——>。
+		text = text.replace(new RegExp("([{CJK}]),".replace("{CJK}", CJK), "g"), '$1，');// ,——>，
+		text = text.replace(new RegExp("([{CJK}]);".replace("{CJK}", CJK), "g"), '$1；');// ;——>；
+		text = text.replace(new RegExp("([{CJK}])!".replace("{CJK}", CJK), "g"), '$1！');// \!——>！
+		text = text.replace(new RegExp("([{CJK}])\?".replace("{CJK}", CJK), "g"), '$1？');// \?——>？
+		text = text.replace(new RegExp("([{CJK}])\:".replace("{CJK}", CJK), "g"), '$1：');// :——>：
+		text = text.replace(new RegExp("(\")([{CJK}]*)(\")".replace("{CJK}", CJK), "g"), '“$2”');// "" ——> “”
+		text = text.replace(new RegExp("[^\\]](\\()([{CJK}]*)(\\))".replace("{CJK}", CJK), "g"), '（$2）');// () ——> （）
 
-		// 使用直角引号
-		if(config.get('covert_quotations')){
-			content = content.replace(/‘/g, "『");
-			content = content.replace(/’/g, "』");
-			content = content.replace(/“/g, "「");
-			content = content.replace(/”/g, "」");
+		// 连续三个以上的句号 ——> ......
+		text = text.replace(/(。|\.){3,}/g, '......');
+		// 多个标点符号 ——> 一个标点符号（！？及？！算一个）
+		text = text.replace(/(！？|？！|[！？。，；：、“”「」『』〖〗《》【】])(\1|[！？。，；：、“”「」『』〖〗《》【】]){1,}/g, '$1');
+
+		let config = this.config
+		if (config.get('covert_Chinese_quotations')) {
+			// 中文引号 ——> 直角引号
+			text = text.replace(/‘/g, "『");
+			text = text.replace(/’/g, "』");
+			text = text.replace(/“/g, "「");
+			text = text.replace(/”/g, "」");
+		}
+		if(config.get('covert_English_quotations')){
+			// 英文引号 ——> 直角引号
+			text = text.replace(/(")([^"]*)(")/g, "「$2」")
+			text = text.replace(/(')([^']*)(')/g, "『$2』")
 		}
 
-		// 使用半角括号
-		// 半角括号的两边都有空格就不在这里处理了，放到行中处理
-		if(config.get('covert_brackets_to_halfwidth')){
-			content = content.replace(/\s*[（\(]\s*/g, " ( ");
-			content = content.replace(/\s*[）\)]\s*/g, " ) ");
+		/* 完整的英文整句、特殊名词，其内容使用半角标点 */
+		//TODO：实现替换对应字符而不需要自己输入
+		console.log("标点替换结束，下列操作需手动完成：")
+		console.log("1. 完整的英文整句、特殊名词，其内容建议使用半角标点。请在编辑器中开启搜索并手动输入正则匹配检查英文或数字周围的标点是否正确：(\\w+)")
+		console.log("2. 除 1 中指出的位置及 Markdown 标记需使用半角标点以外，其他位置建议使用全角标点。请在编辑器中开启搜索并手动输入正则匹配检查半角标点是否应该转为全角标点：[\\?~:!,\\.'\"\\-;\\(\\)\\[\\]\\{\\}]")
+
+		return text;
+	};
+
+	/* 全角数字 ——> 半角数字 */
+	replaceFullNums(text) {
+		let config = this.config
+		if(config.get('convert_fullwidthNums')){
+			" 全角数字。";
+			text = text.replace(/０/g, "0");
+			text = text.replace(/１/g, "1");
+			text = text.replace(/２/g, "2");
+			text = text.replace(/３/g, "3");
+			text = text.replace(/４/g, "4");
+			text = text.replace(/５/g, "5");
+			text = text.replace(/６/g, "6");
+			text = text.replace(/７/g, "7");
+			text = text.replace(/８/g, "8");
+			text = text.replace(/９/g, "9");
 		}
-
-		// 英文和数字内部的全角标点 ，。；‘’“”：？！＠＃％＆－＝＋｛｝【】｜＼～ 改成半角标点
-		content = content.replace(/(\w)\s*，\s*(\w)/g, "$1, $2");
-		content = content.replace(/(\w)\s*。\s*(\w)/g, "$1. $2");
-		content = content.replace(/(\w)\s*；\s*(\w)/g, "$1; $2");
-		content = content.replace(/(\w)\s*‘\s*(\w)/g, "$1 '$2");
-		content = content.replace(/(\w)\s*’\s*(\w)/g, "$1' $2");
-		content = content.replace(/(\w)\s*“\s*(\w)/g, '$1 "$2');
-		content = content.replace(/(\w)\s*”\s*(\w)/g, '$1" $2');
-		content = content.replace(/(\w)\s*：\s*(\w)/g, "$1: $2");
-		content = content.replace(/(\w)\s*？\s*(\w)/g, "$1? $2");
-		content = content.replace(/(\w)\s*！\s*(\w)/g, "$1! $2");
-		content = content.replace(/(\w)\s*＠\s*(\w)/g, "$1@$2");
-		content = content.replace(/(\w)\s*＃\s*(\w)/g, "$1#$2");
-		content = content.replace(/(\w)\s*％\s*(\w)/g, "$1 % $2");
-		content = content.replace(/(\w)\s*＆\s*(\w)/g, "$1 & $2");
-		content = content.replace(/(\w)\s*－\s*(\w)/g, "$1 - $2");
-		content = content.replace(/(\w)\s*＝\s*(\w)/g, "$1 = $2");
-		content = content.replace(/(\w)\s*＋\s*(\w)/g, "$1 + $2");
-		content = content.replace(/(\w)\s*｛\s*(\w)/g, "$1 {$2");
-		content = content.replace(/(\w)\s*｝\s*(\w)/g, "$1} $2");
-		content = content.replace(/(\w)\s*[【\[]\s*(\w)/g, "$1 [$2");
-		content = content.replace(/(\w)\s*[】\]]\s*(\w)/g, "$1] $2");
-		content = content.replace(/(\w)\s*｜\s*(\w)/g, "$1 | $2");
-		content = content.replace(/(\w)\s*＼\s*(\w)/g, "$1 \ $2");
-		content = content.replace(/(\w)\s*～\s*(\w)/g, "$1~$2");
-		// 连续三个以上的 `。` 改成 `......`
-		content = content.replace(/[。]{3,}/g, '......');
-		// 截断连续超过一个的 ？和！ 为一个，「！？」也算一个
-		content = content.replace(/([！？]+)\1{1,}/g, '$1');
-		// 截断连续超过一个的 。，；：、“”『』〖〗《》 为一个
-		content = content.replace(/([。，；：、“”「」『』〖〗《》【】])\1{1,}/g, '$1');
-		return content;
+		return text;
 	};
 
-	replaceFullNums(content) {
-		" 全角数字。";
-		content = content.replace(/０/g, "0");
-		content = content.replace(/１/g, "1");
-		content = content.replace(/２/g, "2");
-		content = content.replace(/３/g, "3");
-		content = content.replace(/４/g, "4");
-		content = content.replace(/５/g, "5");
-		content = content.replace(/６/g, "6");
-		content = content.replace(/７/g, "7");
-		content = content.replace(/８/g, "8");
-		content = content.replace(/９/g, "9");
-		return content;
+	/* 全角字母 ——> 半角字母 */
+	replaceFullChars(text) {
+		let config = this.config
+		if(config.get('convert_fullwidthChars')){
+			" 全角字母。";
+			text = text.replace(/Ａ/g, "A");
+			text = text.replace(/Ｂ/g, "B");
+			text = text.replace(/Ｃ/g, "C");
+			text = text.replace(/Ｄ/g, "D");
+			text = text.replace(/Ｅ/g, "E");
+			text = text.replace(/Ｆ/g, "F");
+			text = text.replace(/Ｇ/g, "G");
+			text = text.replace(/Ｈ/g, "H");
+			text = text.replace(/Ｉ/g, "I");
+			text = text.replace(/Ｊ/g, "J");
+			text = text.replace(/Ｋ/g, "K");
+			text = text.replace(/Ｌ/g, "L");
+			text = text.replace(/Ｍ/g, "M");
+			text = text.replace(/Ｎ/g, "N");
+			text = text.replace(/Ｏ/g, "O");
+			text = text.replace(/Ｐ/g, "P");
+			text = text.replace(/Ｑ/g, "Q");
+			text = text.replace(/Ｒ/g, "R");
+			text = text.replace(/Ｓ/g, "S");
+			text = text.replace(/Ｔ/g, "T");
+			text = text.replace(/Ｕ/g, "U");
+			text = text.replace(/Ｖ/g, "V");
+			text = text.replace(/Ｗ/g, "W");
+			text = text.replace(/Ｘ/g, "X");
+			text = text.replace(/Ｙ/g, "Y");
+			text = text.replace(/Ｚ/g, "Z");
+			text = text.replace(/ａ/g, "a");
+			text = text.replace(/ｂ/g, "b");
+			text = text.replace(/ｃ/g, "c");
+			text = text.replace(/ｄ/g, "d");
+			text = text.replace(/ｅ/g, "e");
+			text = text.replace(/ｆ/g, "f");
+			text = text.replace(/ｇ/g, "g");
+			text = text.replace(/ｈ/g, "h");
+			text = text.replace(/ｉ/g, "i");
+			text = text.replace(/ｊ/g, "j");
+			text = text.replace(/ｋ/g, "k");
+			text = text.replace(/ｌ/g, "l");
+			text = text.replace(/ｍ/g, "m");
+			text = text.replace(/ｎ/g, "n");
+			text = text.replace(/ｏ/g, "o");
+			text = text.replace(/ｐ/g, "p");
+			text = text.replace(/ｑ/g, "q");
+			text = text.replace(/ｒ/g, "r");
+			text = text.replace(/ｓ/g, "s");
+			text = text.replace(/ｔ/g, "t");
+			text = text.replace(/ｕ/g, "u");
+			text = text.replace(/ｖ/g, "v");
+			text = text.replace(/ｗ/g, "w");
+			text = text.replace(/ｘ/g, "x");
+			text = text.replace(/ｙ/g, "y");
+			text = text.replace(/ｚ/g, "z");
+		}
+		return text;
 	};
 
-	replaceFullChars(content) {
-		" 全角英文和标点。";
-		content = content.replace(/Ａ/g, "A");
-		content = content.replace(/Ｂ/g, "B");
-		content = content.replace(/Ｃ/g, "C");
-		content = content.replace(/Ｄ/g, "D");
-		content = content.replace(/Ｅ/g, "E");
-		content = content.replace(/Ｆ/g, "F");
-		content = content.replace(/Ｇ/g, "G");
-		content = content.replace(/Ｈ/g, "H");
-		content = content.replace(/Ｉ/g, "I");
-		content = content.replace(/Ｊ/g, "J");
-		content = content.replace(/Ｋ/g, "K");
-		content = content.replace(/Ｌ/g, "L");
-		content = content.replace(/Ｍ/g, "M");
-		content = content.replace(/Ｎ/g, "N");
-		content = content.replace(/Ｏ/g, "O");
-		content = content.replace(/Ｐ/g, "P");
-		content = content.replace(/Ｑ/g, "Q");
-		content = content.replace(/Ｒ/g, "R");
-		content = content.replace(/Ｓ/g, "S");
-		content = content.replace(/Ｔ/g, "T");
-		content = content.replace(/Ｕ/g, "U");
-		content = content.replace(/Ｖ/g, "V");
-		content = content.replace(/Ｗ/g, "W");
-		content = content.replace(/Ｘ/g, "X");
-		content = content.replace(/Ｙ/g, "Y");
-		content = content.replace(/Ｚ/g, "Z");
-		content = content.replace(/ａ/g, "a");
-		content = content.replace(/ｂ/g, "b");
-		content = content.replace(/ｃ/g, "c");
-		content = content.replace(/ｄ/g, "d");
-		content = content.replace(/ｅ/g, "e");
-		content = content.replace(/ｆ/g, "f");
-		content = content.replace(/ｇ/g, "g");
-		content = content.replace(/ｈ/g, "h");
-		content = content.replace(/ｉ/g, "i");
-		content = content.replace(/ｊ/g, "j");
-		content = content.replace(/ｋ/g, "k");
-		content = content.replace(/ｌ/g, "l");
-		content = content.replace(/ｍ/g, "m");
-		content = content.replace(/ｎ/g, "n");
-		content = content.replace(/ｏ/g, "o");
-		content = content.replace(/ｐ/g, "p");
-		content = content.replace(/ｑ/g, "q");
-		content = content.replace(/ｒ/g, "r");
-		content = content.replace(/ｓ/g, "s");
-		content = content.replace(/ｔ/g, "t");
-		content = content.replace(/ｕ/g, "u");
-		content = content.replace(/ｖ/g, "v");
-		content = content.replace(/ｗ/g, "w");
-		content = content.replace(/ｘ/g, "x");
-		content = content.replace(/ｙ/g, "y");
-		content = content.replace(/ｚ/g, "z");
-        content = content.replace(/＠/g, "@");
-		return content;
-	};
+	/* 专有名词使用正确的大小写；不使用不地道的缩写 */
+	properNounsAndAbbreviations(){
+		console.log("如果文档中存在专有名词，请自行确认其大小写正确且没有使用不地道的缩写。")
+	}
 };
 
 class Watcher {
@@ -322,4 +328,3 @@ class Watcher {
 		new PanguFormatter().updateDocument();
 	};
 };
-//# sourceMappingURL=extension.js.map
